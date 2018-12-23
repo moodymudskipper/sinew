7c0fa19df47f1ebc970cf5f1f8ebd1f0fa32ccc6
Jonathan Sidi <yonis@metrumrg.com>
2018-12-23 11:28:21 -0500

    add makeImport addin

--- R/oxygenAddin.R
+++ R/oxygenAddin.R
@@ -1,9 +1,25 @@
-# oxygenAddin function [sinew] ---- 
+# oxygenAddin roxy [sinew] ---- 
+#' @importFrom rstudioapi getSourceEditorContext insertText
+# oxygenAddin function [sinew] ----
 oxygenAddin <- function() {
-  context <- rstudioapi::getActiveDocumentContext()
+  context <- rstudioapi::getSourceEditorContext()
   obj_name <- gsub("[\"\']", "", context$selection[[1]]$text)
   assign(obj_name, eval(parse(text = obj_name)))
-  eval(parse(text = sprintf("ret<-makeOxygen(%s,print=FALSE)", obj_name)))
+  eval(parse(text = sprintf("ret<-makeOxygen(%s,print=FALSE,cut=sinew_opts$get('cut'))", obj_name)))
+  cl <- NULL
+  eval(parse(text = sprintf("cl<-class(%s)", obj_name)))
+  if (cl == "function") ret <- paste(ret, obj_name, sep = "\n")
+  rstudioapi::insertText(text = ret)
+}
+
+# importAddin roxy [sinew] ---- 
+#' @importFrom rstudioapi getSourceEditorContext insertText
+# importAddin function [sinew] ----
+importAddin <- function() {
+  context <- rstudioapi::getSourceEditorContext()
+  obj_name <- gsub("[\"\']", "", context$selection[[1]]$text)
+  assign(obj_name, eval(parse(text = obj_name)))
+  eval(parse(text = sprintf("ret<-makeImport(%s,print=FALSE,cut=sinew_opts$get('cut'))", obj_name)))
   cl <- NULL
   eval(parse(text = sprintf("cl<-class(%s)", obj_name)))
   if (cl == "function") ret <- paste(ret, obj_name, sep = "\n")
