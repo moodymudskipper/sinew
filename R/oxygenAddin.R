edf25d2c4e215e9437c2233f67f350fa9e228a2b
moodymudskipper <antoine.fabri@gmail.com>
2020-02-19 16:26:13 +0100

    Add createOxygenLight addin, oxygenLightAddin(), and makeOxygenLight()

--- R/oxygenAddin.R
+++ R/oxygenAddin.R
@@ -8,3 +8,14 @@ oxygenAddin <- function() {
   if (cl == "function") ret <- paste(ret, obj_name, sep = "\n")
   rstudioapi::insertText(text = ret)
 }
+
+oxygenLightAddin <- function() {
+  context <- rstudioapi::getActiveDocumentContext()
+  obj_name <- gsub("[\"\']", "", context$selection[[1]]$text)
+  assign(obj_name, eval(parse(text = obj_name)))
+  eval(parse(text = sprintf("ret<-makeOxygenLight(%s,print=FALSE)", obj_name)))
+  cl <- NULL
+  eval(parse(text = sprintf("cl<-class(%s)", obj_name)))
+  if (cl == "function") ret <- paste(ret, obj_name, sep = "\n")
+  rstudioapi::insertText(text = ret)
+}
